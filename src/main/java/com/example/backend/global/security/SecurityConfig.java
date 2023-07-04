0d92bcdd165a52480ca5622e40cedaf58beb4138
김호영 <81502235+beargame123@users.noreply.github.com>
2023-07-04 19:45:55 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/backend/global/security/SecurityConfig.java
+++ src/main/java/com/example/backend/global/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
 
                 .antMatchers("/.well-known/acme-challenge/**").permitAll()
 
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
 
                 .and()
 
