988d45d62c9d3f3478e988533c683a5f66a929e6
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 00:42:52 +0900

    추가

--- src/main/java/com/example/backend/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/backend/domain/comment/domain/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.example.backend.domain.comment.domain.repository;
 import com.example.backend.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findByCommentId(Long id);
 }
