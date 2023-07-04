3acea397b50626729d29cc44078068f0732ade57
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 02:01:24 +0900

    추가

--- src/main/java/com/example/backend/domain/map/domain/repository/MapRepository.java
+++ src/main/java/com/example/backend/domain/map/domain/repository/MapRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MapRepository extends JpaRepository<Map, Long> {
-    Optional<Map> findMap(String );
+    Optional<Map> findMap(Long mapId);
 }
