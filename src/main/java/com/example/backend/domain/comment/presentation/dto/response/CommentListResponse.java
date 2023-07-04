3732f40b58170d416af40c9d7acdce2a513c369f
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 01:18:59 +0900

    추가

--- src/main/java/com/example/backend/domain/comment/presentation/dto/response/CommentListResponse.java
+++ src/main/java/com/example/backend/domain/comment/presentation/dto/response/CommentListResponse.java
@@ -1,4 +1,14 @@
 package com.example.backend.domain.comment.presentation.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
 public class CommentListResponse {
+    private Long commentId;
+    private String comment;
+    private LocalDate date;
 }
