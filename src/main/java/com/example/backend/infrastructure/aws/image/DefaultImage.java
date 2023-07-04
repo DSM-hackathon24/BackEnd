523ea4c5f9b15a9ac386714be888bb704f9d9cb3
beargame123 <hoyoung7827@gmail.com>
2023-07-04 14:18:43 +0900

    14

---
+++ src/main/java/com/example/backend/infrastructure/aws/image/DefaultImage.java
@@ -0,0 +1,9 @@
+package com.example.backend.infrastructure.aws.image;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DefaultImage {
+    public static final String USER_PROFILE_IMAGE = "https://media.discordapp.net/attachments/822309850700054548/1096753669531250708/afbbc284a5019bb3.png?width=300&height=300";
+}
