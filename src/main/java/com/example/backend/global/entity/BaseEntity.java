5061fc49e89081c8dd7550a44bdc4eddf6d39136
beargame123 <hoyoung7827@gmail.com>
2023-07-04 21:50:18 +0900

    9

--- src/main/java/com/example/backend/global/entity/BaseEntity.java
+++ src/main/java/com/example/backend/global/entity/BaseEntity.java
@@ -1,16 +1,19 @@
 package com.example.backend.global.entity;
 
+import lombok.Getter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
+@Getter
 @MappedSuperclass
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDate createdAt = LocalDate.now();
 }
