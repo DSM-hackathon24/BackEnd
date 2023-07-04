836db1a5ab5650787ce381a3ce337995612c1204
beargame123 <hoyoung7827@gmail.com>
2023-07-04 21:50:12 +0900

    8

---
+++ src/main/java/com/example/backend/domain/board/service/QueryBoardListService.java
@@ -0,0 +1,34 @@
+package com.example.backend.domain.board.service;
+
+import com.example.backend.domain.board.domain.Board;
+import com.example.backend.domain.board.facade.BoardFacade;
+import com.example.backend.domain.board.presentation.dto.response.QueryBoardListResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryBoardListService {
+    private final BoardFacade boardFacade;
+
+    @Transactional(readOnly = true)
+    public List<QueryBoardListResponse> execute() {
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        List<Board> boards = boardFacade.getBoardAllById(sort);
+
+        return boards.stream()
+                .map(board -> QueryBoardListResponse.builder()
+                        .boardId(board.getId())
+                        .title(board.getTitle())
+                        .content(board.getContent())
+                        .date(board.getCreatedAt())
+                        .writer(board.getUser().getNickName())
+                        .build())
+                .toList();
+    }
+}
