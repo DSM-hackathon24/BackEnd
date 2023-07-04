2045895208383695e32058256d55ed4560eff55b
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 01:37:50 +0900

    댓글생성기능추가

--- src/main/java/com/example/backend/domain/comment/presentation/CommentController.java
+++ src/main/java/com/example/backend/domain/comment/presentation/CommentController.java
@@ -1,7 +1,32 @@
 package com.example.backend.domain.comment.presentation;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.backend.domain.comment.domain.Comment;
+import com.example.backend.domain.comment.presentation.dto.request.CommentAddRequest;
+import com.example.backend.domain.comment.service.CreateCommentService;
+import com.example.backend.domain.comment.service.DeleteCommentService;
+import com.example.backend.domain.comment.service.ListCommentService;
+import com.example.backend.domain.comment.service.ModifyCommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
+@RequestMapping("/comment")
 @RestController
 public class CommentController {
+
+    private final CreateCommentService createCommentService;
+    private final DeleteCommentService deleteCommentService;
+    private final ListCommentService listCommentService;
+    private final ModifyCommentService modifyCommentService;
+
+    @Operation(summary = "댓글 생성")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createComment(@RequestBody CommentAddRequest request, Long boardId){
+        createCommentService.addComment(request, boardId);
+    }
+
+    
 }
--- src/main/java/com/example/backend/domain/comment/service/DeleteCommentService.java
+++ src/main/java/com/example/backend/domain/comment/service/DeleteCommentService.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class DeleteCommentRequest {
+public class DeleteCommentService {
 
     private final BoardFacade boardFacade;
     private final UserFacade userFacade;
