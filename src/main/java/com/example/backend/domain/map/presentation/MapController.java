8e34fc6acf27c3002ece54f2d72d9710079a816f
beargame123 <hoyoung7827@gmail.com>
2023-07-04 19:51:25 +0900

    ➕:: Swagger_add

--- src/main/java/com/example/backend/domain/map/presentation/MapController.java
+++ src/main/java/com/example/backend/domain/map/presentation/MapController.java
@@ -2,14 +2,15 @@ package com.example.backend.domain.map.presentation;
 
 import com.example.backend.domain.map.presentation.dto.request.MapAddRequest;
 import com.example.backend.domain.map.service.MapAddService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Tag(name = "Map", description = "Map API 입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/map")
@@ -17,7 +18,9 @@ public class MapController {
 
     private final MapAddService mapAddService;
 
+    @Operation(summary = "소화전 장소 저장")
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void mapAdd(@RequestBody @Valid MapAddRequest request){
         mapAddService.addMap(request);
     }
