744b0b3f19fbc1cfd21b99bafc56e07def5401dc
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 01:25:44 +0900

    수정

--- src/main/java/com/example/backend/domain/comment/facade/CommentFacade.java
+++ src/main/java/com/example/backend/domain/comment/facade/CommentFacade.java
@@ -1,11 +1,15 @@
 package com.example.backend.domain.comment.facade;
 
+import com.example.backend.domain.board.domain.Board;
 import com.example.backend.domain.comment.domain.Comment;
 import com.example.backend.domain.comment.domain.repository.CommentRepository;
 import com.example.backend.global.error.ErrorCode;
 import com.example.backend.global.error.exeception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -16,5 +20,9 @@ public class CommentFacade {
     public Comment getCommentById(Long id){
         return commentRepository.findByCommentId(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+    }
+
+    public List<Comment> getCommentAllById(Sort sort){
+        return commentRepository.findAll(sort);
     }
 }
