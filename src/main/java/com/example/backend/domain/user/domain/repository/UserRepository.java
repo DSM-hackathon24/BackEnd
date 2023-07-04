923e149a24f957719efbcd5ba72ab97dead7d67b
beargame123 <hoyoung7827@gmail.com>
2023-07-04 14:16:45 +0900

    1

---
+++ src/main/java/com/example/backend/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.backend.domain.user.domain.repository;
+
+import com.example.backend.domain.user.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findUserByAccountId(String accountId);
+
+    Boolean existsByAccountId(String accountId);
+
+    boolean existsByEmail(String email);
+}
