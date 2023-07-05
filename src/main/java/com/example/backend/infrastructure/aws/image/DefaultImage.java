e5a1dc137be5286030daa6e6ed8c94a1ec633c04
beargame123 <hoyoung7827@gmail.com>
2023-07-05 15:17:09 +0900

    1

--- src/main/java/com/example/backend/infrastructure/aws/image/DefaultImage.java
+++ src/main/java/com/example/backend/infrastructure/aws/image/DefaultImage.java
@@ -5,5 +5,5 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultImage {
-    public static final String USER_PROFILE_IMAGE = "https://media.discordapp.net/attachments/822309850700054548/1096753669531250708/afbbc284a5019bb3.png?width=300&height=300";
+    public static final String USER_PROFILE_IMAGE = "https://media.discordapp.net/attachments/822309850700054548/1125925911024373791/png-clipart-computer-icons-user-s-included-miscellaneous-user-profile.png?width=1107&height=630";
 }
