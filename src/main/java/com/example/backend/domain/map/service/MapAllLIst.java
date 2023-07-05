dd0e700a55bd0a477862fb223e1f2515799d97bd
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 19:21:05 +0900

    추가

---
+++ src/main/java/com/example/backend/domain/map/service/MapAllLIst.java
@@ -0,0 +1,14 @@
+package com.example.backend.domain.map.service;
+
+import com.example.backend.domain.map.presentation.dto.response.MapListResponse;
+import com.example.backend.domain.user.domain.User;
+import com.example.backend.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MapAllLIst {
+
+
+}
