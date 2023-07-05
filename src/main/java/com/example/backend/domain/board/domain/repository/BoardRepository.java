dcf6596d2583352aedeb1908b79ae27899d7c658
beargame123 <hoyoung7827@gmail.com>
2023-07-05 19:24:28 +0900

    1

--- src/main/java/com/example/backend/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/example/backend/domain/board/domain/repository/BoardRepository.java
@@ -2,9 +2,14 @@ package com.example.backend.domain.board.domain.repository;
 
 import com.example.backend.domain.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Optional<Board> findBoardById(Long id);
+
+    @Query(value = "select b from Board b where b.title LIKE %:kda%")
+    List<Board> findAllBoardByTitleSearch(String kda);
 }
