9f210b76358231b56be0f8b999812616524067b0
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 19:21:16 +0900

    추가

--- src/main/java/com/example/backend/domain/map/service/MapAllLIst.java
+++ src/main/java/com/example/backend/domain/map/service/MapAllLIst.java
@@ -10,5 +10,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MapAllLIst {
 
+    private final UserFacade userFacade;
 
+    public MapListResponse mapAllList(){
+
+        User user = userFacade.getCurrentUser();
+
+        return MapListResponse.builder()
+                .longitude(mapAllList().getLongitude())
+                .latitude(mapAllList().getLatitude())
+                .availability(mapAllList().getAvailability())
+                .installation(mapAllList().getInstallation())
+                .sortation(mapAllList().getSortation())
+                .writer(user.getNickName())
+                .build();
+    }
 }
